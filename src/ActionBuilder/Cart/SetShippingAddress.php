76215e5219e195edb8f96436d2af5e5d8f0a180e
Michel Chowanski <chowanski@bestit-online.de>
2017-02-16 23:18:45 +0100

    Set cart shipping address

---
+++ src/ActionBuilder/Cart/SetShippingAddress.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace BestIt\CommercetoolsODM\ActionBuilder\Cart;
+
+use BestIt\CommercetoolsODM\ActionBuilder\ActionBuilderAbstract;
+use BestIt\CommercetoolsODM\Mapping\ClassMetadataInterface;
+use Commercetools\Core\Model\Cart\Cart;
+use Commercetools\Core\Model\Common\Address;
+use Commercetools\Core\Request\AbstractAction;
+use Commercetools\Core\Request\Carts\Command\CartSetShippingAddressAction;
+
+/**
+ * Builds the action to change cart shipping address
+ * @author chowanski <chowanski@bestit-online.de>
+ * @package BestIt\CommercetoolsODM
+ * @subpackage ActionBuilder\Cart
+ * @version $id$
+ */
+class SetShippingAddress extends ActionBuilderAbstract
+{
+    /**
+     * @inheritdoc
+     */
+    protected $fieldName = 'shippingAddress';
+
+    /**
+     * For which class is this description used?
+     * @var string
+     */
+    protected $modelClass = Cart::class;
+
+    /**
+     * Creates the update action for the given class and data.
+     * @param mixed $changedValue
+     * @param ClassMetadataInterface $metadata
+     * @param array $changedData
+     * @param array $oldData
+     * @param Cart $sourceObject
+     * @param string $subFieldName If you work on attributes etc. this is the name of the specific attribute.
+     * @return AbstractAction[]
+     */
+    public function createUpdateActions(
+        $changedValue,
+        ClassMetadataInterface $metadata,
+        array $changedData,
+        array $oldData,
+        $sourceObject,
+        string $subFieldName = ''
+    ): array {
+        $actions = [];
+
+        if ($changedValue && count($changedValue)) {
+            $action = CartSetShippingAddressAction::of();
+
+            $address = array_merge(
+                $oldData['shippingAddress'] ?? [],
+                $changedValue
+            );
+            $action->setAddress(Address::fromArray($address));
+
+            $actions[] = $action;
+        }
+
+        return $actions;
+    }
+}
