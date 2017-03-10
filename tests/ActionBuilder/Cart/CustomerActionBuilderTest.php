d00278778f644313d7859fc7c920525942aceafa
Michel Chowanski <chowanski@bestit-online.de>
2017-03-10 08:49:48 +0100

    CTPBH-706: Add shipping method action for cart

---
+++ tests/ActionBuilder/Cart/CustomerActionBuilderTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace BestIt\CommercetoolsODM\Tests\ActionBuilder\Cart;
+
+use BestIt\CommercetoolsODM\ActionBuilder\ActionBuilderAbstract;
+use BestIt\CommercetoolsODM\ActionBuilder\Cart\CartActionBuilder;
+use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_MockObject_MockObject;
+
+/**
+ * Tests CartActionBuilder.
+ * @author lange <lange@bestit-online.de>
+ * @category Tests
+ * @package BestIt\CommercetoolsODM
+ * @subpackage ActionBuilder\Cart
+ * @version $id$
+ */
+class CartActionBuilderTest extends TestCase
+{
+    /**
+     * The test class.
+     * @var CartActionBuilder|PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $fixture = null;
+
+    /**
+     * Sets up the test.
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->fixture = static::getMockForAbstractClass(CartActionBuilder::class);
+    }
+
+    /**
+     * Checks the instance of the builder.
+     * @return void
+     */
+    public function testInstance()
+    {
+        static::assertInstanceOf(ActionBuilderAbstract::class, $this->fixture);
+    }
+}
