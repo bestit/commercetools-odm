85badbd0cdd6b96f996a83c8391a3edca2fe829c
Bjoern Lange <lange@bestit-online.de>
2017-07-18 06:43:53 +0200

    Fixed a bug, that a wrong address could be added

--- tests/ActionBuilder/Customer/AddShippingAddressIdsTest.php
+++ tests/ActionBuilder/Customer/AddShippingAddressIdsTest.php
@@ -6,6 +6,7 @@ use BestIt\CommercetoolsODM\ActionBuilder\Customer\CustomerActionBuilder;
 use BestIt\CommercetoolsODM\ActionBuilder\Customer\AddShippingAddressIds;
 use BestIt\CommercetoolsODM\Mapping\ClassMetadataInterface;
 use BestIt\CommercetoolsODM\Tests\ActionBuilder\SupportTestTrait;
+use Commercetools\Core\Model\Common\AddressCollection;
 use Commercetools\Core\Model\Customer\Customer;
 use Commercetools\Core\Request\Customers\Command\CustomerAddShippingAddressAction;
 use PHPUnit\Framework\TestCase;
@@ -86,12 +87,17 @@ class AddShippingAddressIdsTest extends TestCase
     {
         $customer = new Customer();
 
+        $customer->setAddresses(AddressCollection::fromArray([
+            ['id' => $newId1 = uniqid('', true)],
+            ['id' => $newId2 = uniqid('', true)],
+        ]));
+
         $this->fixture->setLastFoundMatch([uniqid(), $field = uniqid()]);
 
         $actions = $this->fixture->createUpdateActions(
             [
-                $newId1 = uniqid('', true),
-                $newId2 = uniqid('', true),
+                $newId1,
+                $newId2,
                 $oldId = uniqid('', true)
             ],
             $this->createMock(ClassMetadataInterface::class),
