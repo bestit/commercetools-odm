2979ceda934ae28829324935542c6313730ddb0b
Bjoern Lange <lange@bestit-online.de>
2017-09-08 11:56:54 +0200

    Added an actionbuilder to add deliveries to an order

---
+++ src/ActionBuilder/Order/AddDelivery.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BestIt\CommercetoolsODM\ActionBuilder\Order;
+
+use BestIt\CommercetoolsODM\Mapping\ClassMetadataInterface;
+use Commercetools\Core\Request\AbstractAction;
+use Commercetools\Core\Request\Orders\Command\OrderAddDeliveryAction;
+
+/**
+ * Adds deliveries to the order.
+ * @author blange <lange@bestit-online.de>
+ * @package BestIt\CommercetoolsODM\ActionBuilder\Order
+ */
+class AddDelivery extends OrderActionBuilder
+{
+    /**
+     * The field name filter.
+     * @var string
+     */
+    protected $complexFieldFilter = '^shippingInfo/deliveries/(\d+)$';
+
+    /**
+     * Creates the update actions for the given class and data.
+     * @param mixed $changedValue
+     * @param ClassMetadataInterface $metadata
+     * @param array $changedData
+     * @param array $oldData
+     * @param mixed $sourceObject
+     * @return AbstractAction[]
+     */
+    public function createUpdateActions(
+        $changedValue,
+        ClassMetadataInterface $metadata,
+        array $changedData,
+        array $oldData,
+        $sourceObject
+    ): array {
+        return [
+            OrderAddDeliveryAction::fromArray($changedValue)
+        ];
+    }
+}
