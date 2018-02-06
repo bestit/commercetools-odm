0e11376a9167aa6fa16a5a7eeb2a126fc1713aa0
Bjoern Lange <lange@bestit-online.de>
2018-02-06 21:57:53 +0100

    Added unittests to check the standard model checker

--- tests/Mapping/ClassMetadataTest.php
+++ tests/Mapping/ClassMetadataTest.php
@@ -1,40 +1,76 @@
 <?php
 
+declare(strict_types=1);
+
 namespace BestIt\CommercetoolsODM\Tests\Mapping;
 
 use BestIt\CommercetoolsODM\Mapping\Annotations\Field;
 use BestIt\CommercetoolsODM\Mapping\ClassMetadata;
 use BestIt\CommercetoolsODM\Mapping\ClassMetadataInterface;
 use BestIt\CommercetoolsODM\Tests\Mapping\ClassMetadata\TestClass;
+use Commercetools\Core\Model\Cart\Cart;
+use Commercetools\Core\Model\Category\Category;
+use Commercetools\Core\Model\Channel\Channel;
+use Commercetools\Core\Model\Customer\Customer;
+use Commercetools\Core\Model\CustomObject\CustomObject;
+use Commercetools\Core\Model\Inventory\InventoryEntry;
 use Commercetools\Core\Model\Order\Order;
+use Commercetools\Core\Model\Product\Product;
+use Commercetools\Core\Model\Product\ProductProjection;
+use Commercetools\Core\Model\ProductType\ProductType;
+use Commercetools\Core\Model\ShippingMethod\ShippingMethod;
+use Commercetools\Core\Model\ShoppingList\ShoppingList;
+use Commercetools\Core\Model\Zone\Zone;
 use PHPUnit\Framework\TestCase;
 use ReflectionClass;
+use function uniqid;
 
 /**
- * Class ClassMetadataTest
+ * Class ClassMetadataTest.
+ *
  * @author blange <lange@bestit-online.de>
- * @category Tests
- * @package BestIt\CommercetoolsODM
- * @subpackage Mapping
- * @version $id$
+ * @package BestIt\CommercetoolsODM\Tests\Mapping
  */
 class ClassMetadataTest extends TestCase
 {
     /**
-     * The tested class.
-     * @var ClassMetadata
+     * @var ClassMetadata|void The tested class.
      */
-    private $fixture = null;
+    private $fixture;
 
     /**
-     * The used object name.
-     * @var string
+     * @var string|void The used object name.
      */
-    private $objectName = '';
+    private $objectName;
+
+    /**
+     * Returns the classes which should be a standard model.
+     *
+     * @return array
+     */
+    public function getStandardModels(): array
+    {
+        return [
+            [Cart::class],
+            [Category::class],
+            [Channel::class],
+            [CustomObject::class],
+            [Customer::class],
+            [InventoryEntry::class],
+            [Order::class],
+            [Product::class],
+            [ProductProjection::class],
+            [ProductType::class],
+            [ShippingMethod::class],
+            [ShoppingList::class],
+            [Zone::class]
+        ];
+    }
 
     /**
      * Sets up the test.
-     * @reteurn void
+     *
+     * @return void
      */
     protected function setUp()
     {
@@ -142,10 +178,38 @@ class ClassMetadataTest extends TestCase
 
     /**
      * Checks the used interface.
+     *
      * @return void
      */
     public function testInterface()
     {
         $this->assertInstanceOf(ClassMetadataInterface::class, $this->fixture);
+    }
+
+    /**
+     * The registered class is no standard model.
+     *
+     * @return void
+     */
+    public function testIsCTStandardModelFalse()
+    {
+        static::assertFalse($this->fixture->isCTStandardModel());
+    }
+
+    /**
+     * The given class should be a standard model.
+     *
+     * @dataProvider getStandardModels
+     *
+     * @param string $class
+     * @param bool $isStandard
+     *
+     * @return void
+     */
+    public function testIsCTStandardModelSuccess(string $class, bool $isStandard = true)
+    {
+        $this->fixture = new ClassMetadata($class);
+
+        static::assertSame($isStandard, $this->fixture->isCTStandardModel());
     }
 }
