d9810b3c73f8e2a3bab3260dd181883a405954c9
Björn Lange <bjoern.lange@bestit-online.de>
2018-06-05 12:47:23 +0200

    Added a check if the keywords array has a real changed text

--- src/ActionBuilder/Product/SetSearchKeywords.php
+++ src/ActionBuilder/Product/SetSearchKeywords.php
@@ -8,6 +8,7 @@ use BestIt\CommercetoolsODM\Mapping\ClassMetadataInterface;
 use Commercetools\Core\Model\Product\LocalizedSearchKeywords;
 use Commercetools\Core\Request\AbstractAction;
 use Commercetools\Core\Request\Products\Command\ProductSetSearchKeywordsAction;
+use function array_filter;
 
 /**
  * Sets the search keywords on variants.
@@ -42,9 +43,31 @@ class SetSearchKeywords extends ProductActionBuilder
     ): array {
         list(, $dataContainer) = $this->getLastFoundMatch();
 
-        return [
-            ProductSetSearchKeywordsAction::ofKeywords(LocalizedSearchKeywords::fromArray($changedValue))
-                ->setStaged($dataContainer === 'staged')
-        ];
+        $actions = [];
+
+        if ($this->hasRealTextChange($changedValue)) {
+            $actions[] = ProductSetSearchKeywordsAction::ofKeywords(LocalizedSearchKeywords::fromArray($changedValue))
+                ->setStaged($dataContainer === 'staged');
+        }
+
+        return $actions;
+    }
+
+    /**
+     * Returns true if an language array has search keywords where the text is changed too!
+     *
+     * @param $changedValue
+     *
+     * @return bool
+     */
+    protected function hasRealTextChange($changedValue): bool
+    {
+        // Has every language ...
+        return (bool)array_filter($changedValue, function (array $langSearchKeywords): bool {
+            // real search keywords changes with a text change
+            return (bool)array_filter($langSearchKeywords, function (array $searchKeyword): bool {
+                return (bool)@$searchKeyword['text'];
+            });
+        });
     }
 }
